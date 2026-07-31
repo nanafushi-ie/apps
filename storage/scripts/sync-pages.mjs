8bb0481560b646138293e6b48d31f197de3eb8f1
Kazuyuki FUJITA <kekure173@gmail.com>
2026-07-31 22:02:19 +0900

    Update sync-pages.mjs

--- storage/scripts/sync-pages.mjs
+++ storage/scripts/sync-pages.mjs
@@ -8,3 +8,5 @@ await cp("dist/icons", "icons", { recursive: true, force: true });
 await copyFile("dist/index.source.html", "index.html");
 await copyFile("dist/favicon.svg", "favicon.svg");
 await copyFile("dist/og.png", "og.png");
+await copyFile("dist/cover-desktop.png", "cover-desktop.png");
+await copyFile("dist/cover-mobile.png", "cover-mobile.png");
